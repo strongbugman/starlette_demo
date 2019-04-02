4861a7d115fb552066bf9ca5e022dbef83a4bc66
strongbugman <strongbugman@gmail.com>
2019-04-02 13:27:14 +0800

    Add base model

--- app/models.py
+++ app/models.py
@@ -1,22 +1,18 @@
 import typing
 
-from pydantic import BaseModel
+from pydantic import BaseModel as _BaseModel
 
 from . import extensions as exts
 
 
-class Cat(BaseModel):
-    name: str
+class BaseModel(_BaseModel):
     id: int = 0
-    age: int = 0
 
     @classmethod
     def get_db_define(cls) -> str:
         return f"""
         CREATE TABLE {cls.__name__} (
             id      serial PRIMARY KEY,
-            name    varchar(32) DEFAULT '',
-            age     integer DEFAULT 0
         );
         """
 
@@ -30,28 +26,43 @@ class Cat(BaseModel):
             self.id = await exts.db.insert(self.__class__.__name__, data)
 
     @classmethod
-    async def get(cls, _id) -> "Cat":
+    async def get(cls, _id: int) -> "BaseModel":
         result = (
             await exts.db.fetch(
-                f"SELECT {','.join(cls.__annotations__.keys())} FROM cat WHERE id = {exts.db.parse(_id)};"
+                f"SELECT {','.join(cls.__fields__.keys())} FROM {cls.__name__} WHERE id = {exts.db.parse(_id)};"
             )
         )[0]
         return cls(**result)
 
     @classmethod
-    async def delete(cls, _id) -> None:
+    async def delete(cls, _id: int) -> None:
         await exts.db.execute(
             f"DELETE FROM {cls.__name__} WHERE id = {exts.db.parse(_id)};"
         )
 
     @classmethod
     @exts.cache.cached()
-    async def list(cls, page=1, count=20) -> typing.List["Cat"]:
+    async def list(cls, page: int = 1, count: int = 20) -> typing.List["BaseModel"]:
         results = await exts.db.fetch(
-            f"SELECT {','.join(cls.__annotations__.keys())} FROM {cls.__name__} ORDER BY id ASC "
+            f"SELECT {','.join(cls.__fields__.keys())} FROM {cls.__name__} ORDER BY id ASC "
             f"LIMIT {count} OFFSET {(page - 1) * count}"
         )
         return [cls(**result) for result in results]
+
+
+class Cat(BaseModel):
+    name: str
+    age: int = 0
+
+    @classmethod
+    def get_db_define(cls) -> str:
+        return f"""
+        CREATE TABLE {cls.__name__} (
+            id      serial PRIMARY KEY,
+            name    varchar(32) DEFAULT '',
+            age     integer DEFAULT 0
+        );
+        """
 
 
 MODELS = {Cat}
